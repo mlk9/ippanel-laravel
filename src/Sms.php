f4d4d63eaa4c8cac5b5563cce0a8f9097f196e1e
Mohammad Maleki <malekii24@outlook.com>
2021-09-09 10:47:28 +0430

    Create Sms class

---
+++ src/Sms.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Mlk9\Sms;
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class Sms
+{
+    protected $connection;
+    protected $param;
+
+    public function __construct($username,$password,$from)
+    {
+        $this->connection = [
+            'uname'=>$username,
+            'pass'=>$password,
+            'from'=>$from,
+        ];
+
+        return $this;
+    }
+
+    public function message($message,$to = [])
+    {
+        $this->param = array_merge($this->connection,[
+            'to'=>json_encode($to),
+            'op'=>'send',
+        ]);
+        return $this;
+    } 
+
+    public function patternMessage($message,$code,$data = [],$to = [])
+    {
+        $this->param = array_merge($this->connection,[
+            'to'=>json_encode($to),
+            'pattern_code'=>$code,
+            'input_data'=>$data,
+            'op'=>'sendPattern',
+        ]);
+        return $this;
+    } 
+
+    public function send()
+    {
+        try {
+            $client = new Client();
+            $response = $client->request('POST', 'https://ippanel.com/services.jspd', $this->param);
+            return $response;
+        }
+        catch (RequestException $e) 
+        {   
+            return $e;
+        }
+
+        return null;
+    } 
+
+}
